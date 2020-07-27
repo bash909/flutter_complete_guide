a80195cef179219ec6a6fd77682e8563c24a7485
bash909 <basbashar123@outlook.com>
2020-07-27 19:52:19 +0530

    First Commmit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
+  @override
+  var questions = [
+    'What\'s your favourite color?',
+    'What\'s your favourite animal?'
+  ];
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => print('Answer 1 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () {
+                print('Answer 2 rocks!');
+              },
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
